31fbb4dbdca58d54bd1158dd79d6aed71c944ed5
Hossam Ahmed <hossamthapit0@gmail.com>
2024-01-22 16:41:42 +0200

    ADD swap button

--- MoveIt/src/app/Components/search-bar/search-bar.component.ts
+++ MoveIt/src/app/Components/search-bar/search-bar.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule,HttpClientModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
 
+  fromDate!: Date; // You can use a Date object if needed
+  toDate!: Date;
+  searchBar!: FormGroup;
+  
+  
+  constructor(private formBuilder: FormBuilder){
+    this.searchBar = this.formBuilder.group({
+      fromLocation: ['Egypt, Assiut',[Validators.required]],
+      toLocation: ['USA, New YORK',[Validators.required]],
+      fromDate: [new Date(),[Validators.required]],
+      toDate: [new Date(),[Validators.required]],
+      passengers: [1,[Validators.required]],
+    });
+    
+  }
+  
+  ngOnInit(){
+    
+  }
+  
+  search() {
+    console.log("Called: SearchBarComponent:Search() searchBar form data:",this.searchBar.value);
+
+    if(!this.searchBar.valid){
+      console.log(this.searchBar.value);
+      alert("Please Enter valid data");
+    }
+    else {
+
+    }
+  }
+
+  swapLocations() {
+      const fromLocation = this.searchBar.get('fromLocation')?.value;
+      const toLocation = this.searchBar.get('toLocation')?.value;
+      this.searchBar.patchValue({fromLocation: toLocation, toLocation: fromLocation});
+    }
+      
+  
+  
 }
