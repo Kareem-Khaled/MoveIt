979737352a2a803ad241fdcea7edd96e67fedc69
Kareem Khaled <kareemkhaled143@gmail.com>
2024-01-22 20:12:38 +0200

    CREATE trip card

---
+++ MoveIt/src/app/Components/trip-list/trip-list.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-list.component.html',
+  styleUrl: './trip-list.component.css'
+})
+export class TripListComponent {
+
+}
