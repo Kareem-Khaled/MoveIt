15d006002f242dc7bd513a07974a4063ee0cc3b5
Hossam Ahmed <hossamthapit0@gmail.com>
2024-01-23 14:16:51 +0200

    ADD Filter
    ADD Filter component
    FIX some color for search component

--- MoveIt/src/app/app.component.ts
+++ MoveIt/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SearchBarComponent } from './Components/search-bar/search-bar.component';
 import { TripListComponent } from './Components/trip-list/trip-list.component';
+import { FilterComponent } from './Components/filter/filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,SearchBarComponent, TripListComponent],
+  imports: [CommonModule, RouterOutlet,SearchBarComponent, TripListComponent,FilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
