517aa0d807912b88cba04fdbfd9622ada0801e66
Kareem Khaled <kareemkhaled143@gmail.com>
2024-01-23 14:14:40 +0200

    CREATE filter calendar

--- MoveIt/src/app/app.component.ts
+++ MoveIt/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SearchBarComponent } from './Components/search-bar/search-bar.component';
 import { TripListComponent } from './Components/trip-list/trip-list.component';
+import { HeaderComponent } from './Components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,SearchBarComponent, TripListComponent],
+  imports: [CommonModule, RouterOutlet, TripListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
